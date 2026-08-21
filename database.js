9a41f2b5ed52d27a482a70b1470c8630d149ad38
mwanzilewis <mwanzilewis@gmail.com>
2026-08-21 13:40:05 +0300

    Delete database.js

--- database.js
+++
@@ -1,7 +0,0 @@
-// Production would be Redis / Postgres
-// Status: NOT_CHECKED_IN -> PRINT_PENDING -> CHECKED_IN
-
-const db = new Map(); // attendeeId -> { status, jobId, name, timestamps }
-const jobs = new Map(); // jobId -> attendeeId
-
-module.exports = { db, jobs };
